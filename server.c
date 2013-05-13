5e4c3f61502a4a81624a516653d05740cf2dc26c
Tomáš Pružina <pruzinat@gmail.com>
2013-05-14 00:17:13 +0200

    debug printf fix: use DEBUG_PRINT() macro 	modified:   server.c

--- server.c
+++ server.c
@@ -41,9 +41,7 @@ reply_to_method_call(DBusMessage* msg, DBusConnection* conn)
 		fprintf(stderr, "Argument is not string!\n");
 	else
 		dbus_message_iter_get_basic(&args, &param);
-#ifdef DEBUG
-	fprintf(stdout, "Method called with arguments \"%s\"\n", param);
-#endif
+	DEBUG_PRINT("Method called with arguments \"%s\"\n", param);
 	/***************************************************************
 	 ************* PARSE && PROCESS *******************************/
 	if(!is_there_kernel_support())
